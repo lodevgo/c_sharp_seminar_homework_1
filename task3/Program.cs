3fee1abde2599d284c69ec120ffa6fb70650048a
logodev <pumpgem@gmail.com>
2024-07-23 11:15:18 +0300

    task3 has been solved

---
+++ task3/Program.cs
@@ -0,0 +1,13 @@
+﻿//  на вход принимает число number и выводит, является ли число чётным (делится ли оно на два без остатка).
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a % 2 == 0)
+{
+    Console.WriteLine("Число четное");
+}
+// else 
+//     Console.WriteLine("Число нечетное");
+if (a % 2 != 0)
+{
+    Console.WriteLine("Число нечетное");
+}
