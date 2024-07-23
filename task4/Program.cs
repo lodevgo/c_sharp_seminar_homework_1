0317d1f6e0bfa6a034922eca47a89ee99f77dd35
logodev <pumpgem@gmail.com>
2024-07-23 11:54:44 +0300

    I have done a task4

---
+++ task4/Program.cs
@@ -0,0 +1,16 @@
+﻿// на вход принимает число (number), а на выходе выводит все чётные числа от 1 до number (включительно), 
+// после каждого числа должен быть знак пробела.
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; a >= i; i+=2)
+{
+    Console.Write(i + " ");
+}
+
+
+// while (i <= a)
+// {
+//     Console.Write(i + " ");
+//     i = i + 2;
+// }
