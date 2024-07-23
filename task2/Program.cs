a0248f2f1c95c82728601f5a83092ad0611f6bef
logodev <pumpgem@gmail.com>
2024-07-23 11:01:14 +0300

    task 2 has done

---
+++ task2/Program.cs
@@ -0,0 +1,19 @@
+﻿//  на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Введите значение a:");
+int firstValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение b:");
+int secondValue = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите значение c:");
+int thirdValue = Convert.ToInt32(Console.ReadLine());
+int max = firstValue;
+if (secondValue > max)
+{
+max = secondValue;
+}
+if (thirdValue > max)
+{
+max = thirdValue;
+}
+Console.WriteLine($"Максимальное число: {max}");
+
